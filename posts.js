237ab51f314fd9f22722680a1af9c29b126fc01c
Isaac Kennedy Barbosa Andrade <95106435+IsaacAndra@users.noreply.github.com>
2022-03-22 12:13:26 -0300

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,26 @@
+module.exports =
+{
+    posts: [
+        {id: "fafdad",
+        title: "Teste do Mural",
+        description: "Descrição teste"
+        },
+    ],
+
+    getAll()
+    { 
+        return this.posts;
+    },
+
+    newPost(title,description)
+    {
+        this.posts.push({id: generateID(), title, description});
+    }
+
+
+}
+
+function generateID()
+{
+    return Math.random().toString(36).substring(2,9);
+}
